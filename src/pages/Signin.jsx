81a826804bd815f8a96ca83210c2738a2a1e3c0a
Abdurahmono4 <olimjonovrahmonjon7@gmail.com>
2024-04-21 13:58:50 +0500

    new ability to

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,15 +1,27 @@
 import { useSignup } from "../hooks/useSignup";
 import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
 
 function Signup() {
   const { signupWithGoogle, user, error } = useSignup();
 
   return (
     <div className="min-h-screen grid place-items-center">
-      <button onClick={signupWithGoogle} className="btn btn-secondary">
-        <FcGoogle className="w-10 h-10 mr-2 " />
-        <span className="text-2xl">Google</span>
-      </button>
+      <div className="max-w-96  w-full items-center">
+        <button
+          onClick={signupWithGoogle}
+          className="btn btn-secondary w-full mb-5"
+        >
+          <FcGoogle className="w-10 h-10 mr-2 " />
+          <span className="text-2xl">Google</span>
+        </button>
+        <p className="text-center">
+          If you don't have account
+          <Link to="/signup" className="link text-cyan-500">
+            Signup
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -6,7 +6,7 @@ function Signup() {
   console.log(user);
   return (
     <div className="min-h-screen grid place-items-center">
-      <div className="max-w-96  w-full">
+      <div className="max-w-96  w-full items-center">
         <button
           onClick={signupWithGoogle}
           className="btn btn-secondary w-full mb-5"
@@ -14,9 +14,11 @@ function Signup() {
           <FcGoogle className="w-10 h-10 mr-2 " />
           <span className="text-2xl">Google</span>
         </button>
-        <p>
+        <p className="text-center">
           Are you alredy registered?
-          <Link to="/signin">Login</Link>
+          <Link to="/signin" className="link text-cyan-500">
+            Signin
+          </Link>
         </p>
       </div>
     </div>
