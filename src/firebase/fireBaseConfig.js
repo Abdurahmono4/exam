3434b21ba885b5172cec02cae1ec41d63e896230
Abdurahmono4 <olimjonovrahmonjon7@gmail.com>
2024-04-21 12:57:43 +0500

    new change

--- src/firebase/fireBaseConfig.js
+++ src/firebase/fireBaseConfig.js
@@ -1,15 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDT5tQcCyAqyllRfgTAAk-HrH75GU6kY1o",
-  authDomain: "mykitchen-6098c.firebaseapp.com",
-  projectId: "mykitchen-6098c",
-  storageBucket: "mykitchen-6098c.appspot.com",
-  messagingSenderId: "894970247468",
-  appId: "1:894970247468:web:a99b7a3185e030051b1489",
-  measurementId: "G-BBW57R3NE1",
+  apiKey: "AIzaSyAYYtwIhzwI70-WJs76Fll4BIE3QVip2R8",
+  authDomain: "exam-7ded0.firebaseapp.com",
+  projectId: "exam-7ded0",
+  storageBucket: "exam-7ded0.appspot.com",
+  messagingSenderId: "468000617983",
+  appId: "1:468000617983:web:6313c08e59c59e6349e3ca",
+  measurementId: "G-E8XSZVC6D3",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 //Auth
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,16 +1,21 @@
 import { useSignup } from "../hooks/useSignup";
 import { FcGoogle } from "react-icons/fc";
-
+import { Link } from "react-router-dom";
 function Signup() {
   const { signupWithGoogle, user, error } = useSignup();
   console.log(user);
   return (
     <div className="min-h-screen grid place-items-center">
-      <button onClick={signupWithGoogle} className="btn btn-secondary">
-        <FcGoogle className="w-10 h-10 mr-2 " />
-        <span className="text-2xl">Google</span>
-      </button>
-      <p>Are you alredy registre</p>
+      <div>
+        <button onClick={signupWithGoogle} className="btn btn-secondary">
+          <FcGoogle className="w-10 h-10 mr-2 " />
+          <span className="text-2xl">Google</span>
+        </button>
+        <p>
+          Are you alredy registre
+          <Link to="/signin">Login</Link>
+        </p>
+      </div>
     </div>
   );
 }
