c71a1cdc5d810816608362260649b8013da2fce5
Abdurahmono4 <olimjonovrahmonjon7@gmail.com>
2024-04-22 20:25:54 +0500

    new change this website

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -21,17 +21,17 @@ const links = [
 function NavLinks() {
   return (
     <>
-     <Link className="px-3 py-2 hover:bg-base-content hover:text-white rounded-lg "
+     <Link className=" navlink px-3 py-2 hover:bg-base-content hover:text-white rounded-lg "
             to={"/"}
           >
             Home
           </Link>
-          <Link className="px-3 py-2 hover:bg-base-content hover:text-white rounded-lg "
+          <Link className=" navlink px-3 py-2 hover:bg-base-content hover:text-white rounded-lg "
             to={"/create"}
           >
             Create
           </Link>
-          <Link className="px-3 py-2 hover:bg-base-content hover:text-white rounded-lg "
+          <Link className=" navlink px-3 py-2 hover:bg-base-content hover:text-white rounded-lg "
             to={"/contact"}
           >
             Contact
